0d172e88456608a25a1b51bc10e172b42b0a0d6c
Alim <orken77@yandex.kz>
2023-03-06 23:43:58 +0600

    review 1

--- src/Main.java
+++ src/Main.java
@@ -7,37 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
         //System.out.println("Поехали!");
-       /* TaskManager manager = Managers.getDefault();
-        Task taskFirst = new Task ("Сходить в школу", "Отсидеть уроки", NEW);
-        Task taskSecond = new Task("Поехать на работу", "Сесть в автобус", DONE);
-        Epic epicFirst = new Epic("Уборка дома", "Убраться в своей комнате и убраться на кухне", "");
-        Subtask subtaskFirst = new Subtask("Убрать свою комнату", "Заправить постель, выкинуть мусор, вытереть пыль", NEW, 3);
-        Subtask subtaskSecond = new Subtask("Убратьс на кухне", "Помыть посуду, помыть полы", DONE, 3);
-        Epic epicSecond = new Epic("Сделать проект", "Сдать проект до конца недели");
-        Subtask subtaskThird = new Subtask("Кодинг", "Написать бекенд трекера задач", NEW, 4);
-        manager.buildTask(taskFirst);
-        manager.buildTask(taskSecond);
-        manager.buildEpic(epicFirst);
-        manager.buildEpic(epicSecond);
-        manager.buildSubtask(subtaskFirst);
-        manager.buildSubtask(subtaskSecond);
-        manager.buildSubtask(subtaskThird);
-        manager.receivingSubtasks(5);
-        manager.receivingSubtasks(6);
-        manager.receivingTasks(1);
-        manager.receivingTasks(1);
-        manager.receivingTasks(1);
-        manager.receivingEpics(3);
-        manager.receivingTasks(1);
-        manager.receivingEpics(3);
-        manager.receivingTasks(1);
-        manager.receivingEpics(3);
-        manager.receivingSubtasks(5);
-        manager.receivingSubtasks(5);
-        System.out.println(manager.getHistory());
-        System.out.println(manager.getTasks());
-        System.out.println(manager.getEpics());
-        System.out.println(manager.getSubtasks()); */
         TaskManager manager = Managers.getDefault();
         Task taskFirst = new Task ("Сходить в школу", "Отсидеть уроки", NEW);
         Task taskSecond = new Task("Поехать на работу", "Сесть в автобус", DONE);
